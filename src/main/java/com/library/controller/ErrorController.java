8fa9002b634b9756d45fa94b2dc4811bc0f4ead9
mrgeneric <andrewmcneill1992@gmail.com>
2019-05-20 13:54:28 +0100

    finished commenting error controller

--- src/main/java/com/library/controller/ErrorController.java
+++ src/main/java/com/library/controller/ErrorController.java
@@ -7,16 +7,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Simple controller for custom error handling
+ */
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    /**
+     * the get error path method
+     * @return the error method runs
+     */
     @Override
     public String getErrorPath() {
         return "/error";
     }
 
+    /**
+     * The error mapping for custom errors
+     * @param request the http request from client
+     * @return appropriate error page
+     */
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        //The errors for 404 and 500 have been customised below
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
